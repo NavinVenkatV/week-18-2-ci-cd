f763d0c5d27f2613279c1e860e1d167245854fed
NavinVenkatV <vnavinvenkat@gmail.com>
2025-02-17 12:42:17 +0530

    commit

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -7,6 +7,6 @@ import { useBalance } from "@repo/store/balance";
 export default function() {
   const balance = useBalance();
   return <div>
-    hi there {balance}
+    hi bro here is your balance {balance}
   </div>
 }
